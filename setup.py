e7bc4691b40f244ddf620a5cff4909dcf8b62160
Patrick Foldenauer <6030018+patfo@users.noreply.github.com>
2026-07-27 15:52:10 +0200

    Update package metadata in setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
     name='snudd',
-    url='https://github.com/dwpamaral/SNuDD.git',
-    version="1.0",
-    author='Dorian Amaral',
+    url='https://github.com/SNuDD/SNuDD.git',
+    version="0.1",
+    author='Dorian Amaral, David Cerdeno, Andrew Cheek, Patrick Foldenauer',
     packages=['snudd'],
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
