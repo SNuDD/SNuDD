d1efadee96633c8e3b45ba4e614d53c3aede746d
Patrick Foldenauer <patrick.foldenauer@csic.es>
2026-07-27 17:17:49 +0200

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='snudd',
     url='https://github.com/SNuDD/SNuDD.git',
-    version="1.0",
+    version="1.0.0",
     author='Dorian Amaral, David Cerdeno, Andrew Cheek, Valeria Costa, Patrick Foldenauer',
     packages=['snudd'],
-    # packages=setuptools.find_namespace_packages(),
-    # long_description=open('README.txt').read(),
+    packages=find_packages(),
 )
